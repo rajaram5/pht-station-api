1597fba1485c24f0c57971879e9721ecb287aef6
Rajaram <rr.kaliyaperumal@gmail.com>
2019-08-27 16:54:01 +0200

    host added

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def getDataAccessInterface():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    app.run(host='0.0.0.0', debug=True, port=5000) #run app in debug mode on port 5000
